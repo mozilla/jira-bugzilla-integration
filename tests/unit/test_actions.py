1ccd711e4b0ee5bf9b5d565a7a00252c39f8d1fb
Bryan Sieber <bsieber@mozilla.com>
2022-02-14 14:28:12 -0800

    Updating tests

--- tests/unit/test_actions.py
+++ tests/unit/test_actions.py
@@ -4,7 +4,7 @@ import pytest
 from src.core import actions as test_actions
 
 
-def test_actions_context():
+def test_valid_actions_context_succeeds():
     for (
         key
     ) in (
@@ -13,7 +13,8 @@ def test_actions_context():
         assert test_actions.get_action_context_by_key(key=key)
 
 
-def test_actions_context_fails():
+def test_invalid_actions_context_fails():
+    original_dict = test_actions.module_dict.copy()
     test_actions.module_dict["mock"] = None
     with pytest.raises(AssertionError):
         for (
@@ -22,3 +23,4 @@ def test_actions_context_fails():
             test_actions.module_dict.keys()  # pylint: disable=consider-iterating-dictionary
         ):
             assert test_actions.get_action_context_by_key(key=key)
+    test_actions.module_dict = original_dict  # reset globals
