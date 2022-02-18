2b0d02f5f6c92aaafd570e08579c0cbd8bbedf9a
Bryan Sieber <bsieber@mozilla.com>
2022-02-17 18:42:09 -0800

    Minor stylistic adjustment

--- tests/unit/jbi/test_jbi.py
+++ tests/unit/jbi/test_jbi.py
@@ -1,18 +1,18 @@
 # pylint: disable=cannot-enumerate-pytest-fixtures
 import pytest
 
-from src.core import configurator
-from src.jbi import configuration
+from src.core import configurator as test_configurator
+from src.jbi import configuration as jbi_configuration
 
 
 def test_mock_jbi_files():
     with pytest.raises(AssertionError):
-        configurator.process_all_files_in_path(
-            process=configuration.jbi_config_process,
+        test_configurator.process_all_files_in_path(
+            process=jbi_configuration.jbi_config_process,
             folder_path="tests/unit/jbi/mock_jbi_files",
         )
 
 
 def test_actual_jbi_files():
-    jbi_map = configuration.jbi_config_map()
+    jbi_map = jbi_configuration.jbi_config_map()
     assert jbi_map
