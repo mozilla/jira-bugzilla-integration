840def54f60e9e1258818a7f474b5f2741e76bbd
Bryan Sieber <bsieber@mozilla.com>
2022-02-23 14:30:21 -0800

    Updating port based on review commment

--- infra/gunicorn_conf.py
+++ infra/gunicorn_conf.py
@@ -15,7 +15,7 @@ if max_workers_str:
 web_concurrency_str = os.getenv("WEB_CONCURRENCY", None)
 
 host = os.getenv("HOST", "0.0.0.0")
-port = os.getenv("PORT", "80")
+port = os.getenv("PORT", "8000")
 bind_env = os.getenv("BIND", None)
 use_loglevel = os.getenv("LOG_LEVEL", "info")
 if bind_env:
