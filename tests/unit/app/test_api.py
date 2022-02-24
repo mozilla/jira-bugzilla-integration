a5574a3a1ca5d2cb5b0c714e58a90fc2fc46bbdc
Bryan Sieber <bsieber@mozilla.com>
2022-02-23 21:28:44 -0800

    Adding log.py to capture logging setup and configuration.

--- tests/unit/app/test_api.py
+++ tests/unit/app/test_api.py
@@ -1,3 +1,8 @@
+from fastapi.testclient import TestClient
+
+from src.app.api import app
+
+
 def test_read_root(anon_client):
     """The site root redirects to the Swagger docs"""
     resp = anon_client.get("/")
@@ -8,12 +13,14 @@ def test_read_root(anon_client):
     assert prev_resp.headers["location"] == "./docs"
 
 
-def test_request_summary_is_logged(anon_client, caplog):
-    anon_client.get("/__lbheartbeat__")
+def test_request_summary_is_logged(caplog):
+    with TestClient(app) as anon_client:
+        # https://fastapi.tiangolo.com/advanced/testing-events/
+        anon_client.get("/__lbheartbeat__")
 
-    summary = caplog.records[-1]
+        summary = caplog.records[-1]
 
-    assert summary.name == "request.summary"
-    assert summary.method == "GET"
-    assert summary.path == "/__lbheartbeat__"
-    assert summary.querystring == {}
+        assert summary.name == "request.summary"
+        assert summary.method == "GET"
+        assert summary.path == "/__lbheartbeat__"
+        assert summary.querystring == {}
