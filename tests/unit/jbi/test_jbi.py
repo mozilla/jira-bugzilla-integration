f2cffc793483de4f8e431229d17be33b7cf15358
Bryan Sieber <bsieber@mozilla.com>
2022-02-28 17:07:37 -0800

    Adding test-config.yaml

--- tests/unit/jbi/test_jbi.py
+++ tests/unit/jbi/test_jbi.py
@@ -6,7 +6,9 @@ from src.jbi import configuration as jbi_configuration
 
 def test_mock_jbi_files():
     with pytest.raises(jbi_configuration.ProcessError):
-        jbi_configuration.get_yaml_configurations(jbi_config_file="test.yaml")
+        jbi_configuration.get_yaml_configurations(
+            jbi_config_file="tests/unit/jbi/test-config.yaml"
+        )
 
 
 def test_actual_jbi_files():
