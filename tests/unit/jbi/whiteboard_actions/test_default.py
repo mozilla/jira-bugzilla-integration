f7ab8170f95bc18a7f549d88dce73a557d0f9462
Bryan Sieber <bsieber@mozilla.com>
2022-02-28 17:22:26 -0800

    Adding basic tests for default action.

---
+++ tests/unit/jbi/whiteboard_actions/test_default.py
@@ -0,0 +1,22 @@
+# pylint: disable=cannot-enumerate-pytest-fixtures
+import pytest
+
+from src.jbi.whiteboard_actions import default
+
+
+def test_default_invalid_init():
+    with pytest.raises(TypeError):
+        default.init()  # pylint: disable=no-value-for-parameter
+
+
+def test_default_returns_callable_without_data():
+    callable_object = default.init(whiteboard_tag="", jira_project_key="")
+    assert callable_object
+    with pytest.raises(TypeError):
+        assert callable_object()
+
+
+def test_default_returns_callable_with_data():
+    callable_object = default.init(whiteboard_tag="", jira_project_key="")
+    assert callable_object
+    assert callable_object(payload={}, context={})
