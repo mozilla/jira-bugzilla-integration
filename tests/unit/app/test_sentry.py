b717bb77f3588ccdbe0cc7b39e5b85509cd20435
Mathieu Leplatre <mathieu@mozilla.com>
2022-03-17 11:12:59 +0100

    Expect application to raise exceptions

--- tests/unit/app/test_sentry.py
+++ tests/unit/app/test_sentry.py
@@ -7,13 +7,13 @@ import json
 import os.path
 from unittest.mock import patch
 
+import pytest
+
 
 def test_errors_are_reported_to_sentry(anon_client):
     with patch("src.app.api.sentry_sdk.capture_exception") as mocked:
         with patch("src.jbi.router.execute_request", side_effect=ValueError):
-            try:
+            with pytest.raises(ValueError):
                 anon_client.post("/bugzilla_webhook")
-            except Exception:
-                pass  # This shouldn't be necessary, the API should return 500.
 
     assert mocked.called
