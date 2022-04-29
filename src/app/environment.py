95c947ff8d8277d334ec91b3346941214ecadf2a
Bryan Sieber <bsieber@mozilla.com>
2022-04-29 15:16:17 -0700

    Settings builds vars before extracting env vars. Modifying approach to make see_also and jira_remote_link urls

--- src/app/environment.py
+++ src/app/environment.py
@@ -23,13 +23,11 @@ class Settings(BaseSettings):
 
     # Jira
     jira_base_url: str = "https://mozit-test.atlassian.net/"
-    jira_issue_url: str = f"{jira_base_url}browse/%s"
     jira_username: str
     jira_api_key: str
 
     # Bugzilla
     bugzilla_base_url: str = "https://bugzilla-dev.allizom.org"
-    bugzilla_bug_url: str = f"{bugzilla_base_url}/show_bug.cgi?id=%s"
     bugzilla_api_key: str
 
     # Logging
