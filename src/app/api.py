49e28ce4472cc727b51c16c0bebb67d826274eda
Mathieu Leplatre <mathieu@mozilla.com>
2022-06-30 10:57:29 +0200

    Expose config values in root URL (#71)
    * Update version numbers
    
    * Expose configuration in root URL

--- src/app/api.py
+++ src/app/api.py
@@ -8,7 +8,6 @@ from datetime import datetime
 import sentry_sdk
 import uvicorn  # type: ignore
 from fastapi import FastAPI, Request
-from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
@@ -24,7 +23,7 @@ configure_logging()
 app = FastAPI(
     title="Jira Bugzilla Integration (JBI)",
     description="JBI v2 Platform",
-    version="0.1.0",
+    version="2.0.1",
 )
 
 app.include_router(monitor_router)
@@ -39,8 +38,17 @@ app.add_middleware(SentryAsgiMiddleware)
 
 @app.get("/", include_in_schema=False)
 def root(request: Request):
-    """GET via root redirects to /docs."""
-    return RedirectResponse(url="./docs")
+    """Expose key configuration"""
+    return {
+        "title": app.title,
+        "description": app.description,
+        "version": app.version,
+        "documentation": app.docs_url,
+        "configuration": {
+            "jira_base_url": settings.jira_base_url,
+            "bugzilla_base_url": settings.bugzilla_base_url,
+        },
+    }
 
 
 @app.middleware("http")
