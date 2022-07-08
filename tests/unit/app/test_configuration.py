1bd1b8041b10a9722aabf7f48b279ada7503c27c
Mathieu Leplatre <mathieu@mozilla.com>
2022-07-08 09:12:02 +0200

    Add tests for configuration

--- tests/unit/app/test_configuration.py
+++ tests/unit/app/test_configuration.py
@@ -5,14 +5,33 @@ Module for testing src/app/configuration.py functionality
 import pytest
 
 from src.app import configuration
+from src.jbi.models import Actions
 
 
 def test_mock_jbi_files():
     with pytest.raises(configuration.ConfigError) as exc_info:
         configuration.get_actions(jbi_config_file="tests/unit/app/bad-config.yaml")
-    assert str(exc_info.value) == "Errors exist."
+    assert "Errors exist" in str(exc_info.value)
 
 
 def test_actual_jbi_files():
     jbi_map = configuration.get_actions()
     assert jbi_map
+
+
+def test_no_actions_fails():
+    with pytest.raises(ValueError) as exc_info:
+        Actions.parse_obj({"actions": {}})
+    assert "no actions configured" in str(exc_info.value)
+
+
+def test_unknown_module_fails():
+    with pytest.raises(ValueError) as exc_info:
+        Actions.parse_obj({"actions": {"x": {"action": "path.to.unknown"}}})
+    assert "unknown action `path.to.unknown`" in str(exc_info.value)
+
+
+def test_bad_module_fails():
+    with pytest.raises(ValueError) as exc_info:
+        Actions.parse_obj({"actions": {"x": {"action": "src.jbi.runner"}}})
+    assert "action is not properly setup" in str(exc_info.value)
