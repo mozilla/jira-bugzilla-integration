986ef6c6801d3f010e7481e3ef7f4a0ed8630296
Mathieu Leplatre <mathieu@mozilla.com>
2022-07-20 18:42:34 +0200

    Check visible projects in heartbeat (fixes #126)

--- src/jbi/services.py
+++ src/jbi/services.py
@@ -1,12 +1,16 @@
 """Services and functions that can be used to create custom actions"""
-from typing import TypedDict
+import logging
+from typing import Dict, List, TypedDict
 
 import bugzilla as rh_bugzilla
 from atlassian import Jira
 
 from src.app import environment
+from src.jbi.models import Actions
 
 settings = environment.get_settings()
+
+logger = logging.getLogger(__name__)
 
 
 ServiceHealth = TypedDict("ServiceHealth", {"up": bool})
@@ -20,6 +24,12 @@ def get_jira():
         password=settings.jira_api_key,  # package calls this param 'password' but actually expects an api key
         cloud=True,  # we run against an instance of Jira cloud
     )
+
+
+def jira_visible_projects(jira=None) -> List[Dict]:
+    """Return list of projects that are visible with the configured Jira credentials"""
+    jira = jira or get_jira()
+    return jira.projects(included_archived=None)
 
 
 def get_bugzilla():
@@ -36,17 +46,35 @@ def bugzilla_check_health() -> ServiceHealth:
     return health
 
 
-def jira_check_health() -> ServiceHealth:
+def jira_check_health(actions: Actions) -> ServiceHealth:
     """Check health for Jira Service"""
     jira = get_jira()
     server_info = jira.get_server_info(True)
-    health: ServiceHealth = {"up": server_info is not None}
+    health: ServiceHealth = {
+        "up": server_info is not None,
+    }
+    if server_info is None:
+        return health
+
+    visible_projects = {project["key"] for project in jira_visible_projects(jira)}
+    configured_projects = {
+        action.parameters["jira_project_key"]
+        for action in actions.__root__
+        if "jira_project_key" in action.parameters
+    }
+    missing_projects = configured_projects - visible_projects
+    if missing_projects:
+        logger.error(
+            "Projects %s are not visible with configured credentials", missing_projects
+        )
+
+    health["all_projects_are_visible"] = not missing_projects
     return health
 
 
-def jbi_service_health_map():
+def jbi_service_health_map(actions: Actions):
     """Returns dictionary of health check's for Bugzilla and Jira Services"""
     return {
         "bugzilla": bugzilla_check_health(),
-        "jira": jira_check_health(),
+        "jira": jira_check_health(actions),
     }
