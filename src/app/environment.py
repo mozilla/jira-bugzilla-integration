f6a98bf0427f3b8bb8de47f793e85be0a3891357
Graham Beckley <gbeckley@mozilla.com>
2022-07-27 17:28:26 -0400

    Turn app reload off by default

--- src/app/environment.py
+++ src/app/environment.py
@@ -27,7 +27,7 @@ class Settings(BaseSettings):
 
     host: str = "0.0.0.0"
     port: int = 8000
-    app_reload: bool = True
+    app_reload: bool = False
     max_retries: int = 3
     env: Environment = Environment.NONPROD
 
