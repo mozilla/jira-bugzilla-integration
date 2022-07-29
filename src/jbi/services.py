7e97277f58272c14ec1a67e203f9ffe6272be72a
Mathieu Leplatre <mathieu@mozilla.com>
2022-07-29 15:24:58 +0200

    Obtain projects permissions in parallel calls

--- src/jbi/services.py
+++ src/jbi/services.py
@@ -1,4 +1,5 @@
 """Services and functions that can be used to create custom actions"""
+import concurrent.futures
 import logging
 from typing import Dict, List
 
@@ -129,16 +130,29 @@ def _all_jira_projects_visible(jira, actions: Actions) -> bool:
 
 
 def _all_jira_projects_permissions(jira, actions: Actions):
+    all_projects_perms = {}
+    # Query permissions for all configured projects in parallel threads.
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        futures_to_projects = {
+            executor.submit(
+                jira.get_project_permission_scheme, project_key, expand="permissions"
+            ): project_key
+            for project_key in actions.configured_jira_projects_keys
+        }
+        # Obtain futures' results unordered.
+        for future in concurrent.futures.as_completed(futures_to_projects):
+            project_key = futures_to_projects[future]
+            try:
+                response = future.result()
+                all_projects_perms[project_key] = response["permissions"]
+            except Exception as exc:
+                raise exc
+
     required_permissions = {"CREATE_ISSUES", "DELETE_ISSUES", "EDIT_ISSUES"}
-
     misconfigured = []
-
-    for project_key in actions.configured_jira_projects_keys:
-        response = jira.get_project_permission_scheme(project_key, expand="permissions")
-        missing = required_permissions - set(response["permissions"].keys())
-        has_all = all(
-            entry["havePermission"] for entry in response["permissions"].values()
-        )
+    for project_key, permissions in all_projects_perms.items():
+        missing = required_permissions - set(permissions.keys())
+        has_all = all(entry["havePermission"] for entry in permissions.values())
         if missing or not has_all:
             misconfigured.append((project_key, missing))
 
