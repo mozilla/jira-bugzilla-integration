d63b664b314d48dd706aed01a541afbc81af3b2c
Mathieu Leplatre <mathieu@mozilla.com>
2022-08-30 11:09:23 +0200

    Implement Bugzilla REST API client (fixes #189, fixes #198) (#217)
    * Implement Bugzilla REST API client (fixes #189)
    
    * Set api_key param in _call()
    
    * Add requests in pyproject.toml

--- tests/fixtures/bugzilla_action.py
+++ tests/fixtures/bugzilla_action.py
@@ -1,13 +1,4 @@
-from bugzilla import Bugzilla
-from requests import Session
-
-session = Session()
-
-
 class FakeBugzillaAction:
-    def __init__(self, **params):
-        self.bz = Bugzilla(url=None, requests_session=session)
-
     def __call__(self, payload):
         return {"payload": payload}
 
