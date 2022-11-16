7f3b9a98ef67f6f8fd4433d6f0f7035cf07fa4a0
Mathieu Leplatre <mathieu@mozilla.com>
2022-11-16 21:51:40 +0100

    Fix #316: remove jbi_config_file query parameter (#317)

--- tests/unit/test_configuration.py
+++ tests/unit/test_configuration.py
@@ -7,16 +7,23 @@ from jbi import configuration, environment
 
 def test_mock_jbi_files():
     with pytest.raises(configuration.ConfigError) as exc_info:
-        configuration.get_actions(jbi_config_file="tests/fixtures/bad-config.yaml")
+        configuration.get_actions_from_file(
+            jbi_config_file="tests/fixtures/bad-config.yaml"
+        )
     assert "Errors exist" in str(exc_info.value)
 
 
 def test_actual_jbi_files():
-    assert configuration.get_actions(jbi_config_file="config/config.nonprod.yaml")
-    assert configuration.get_actions(jbi_config_file="config/config.prod.yaml")
+    assert configuration.get_actions_from_file(
+        jbi_config_file="config/config.nonprod.yaml"
+    )
+    assert configuration.get_actions_from_file(
+        jbi_config_file="config/config.prod.yaml"
+    )
 
 
 def test_filename_uses_env():
+    configuration.get_actions.cache_clear()
     with mock.patch("jbi.configuration.Actions.parse_file") as mocked:
         configuration.get_actions()
     mocked.assert_called_with("config/config.local.yaml")
