30136917567148730265b89e7b6eca03f899be73
Graham Beckley <gbeckley@mozilla.com>
2023-10-26 16:04:02 -0400

    Add tests for Sentry DSN

--- tests/unit/test_environment.py
+++ tests/unit/test_environment.py
@@ -4,7 +4,10 @@ Pytest overwrites your local environment with the values set by options in
 `tool.pytest.ini_options`
 """
 
-from jbi.environment import Settings, Environment
+import pydantic
+import pytest
+
+from jbi.environment import Environment, Settings
 
 
 def test_settings_env_is_enum_string():
@@ -12,3 +15,12 @@ def test_settings_env_is_enum_string():
 
     assert settings.env == "prod"
     assert str(settings.env) == "prod"
+
+
+def test_sentry_dsn():
+    settings = Settings(sentry_dsn="http://www.example.com/")
+
+
+def test_sentry_dsn_no_url_string_raises():
+    with pytest.raises(pydantic.ValidationError):
+        settings = Settings(sentry_dsn="foobar")
