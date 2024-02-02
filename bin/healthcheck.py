86a3c3b8aa0816fe3ee9ec541e8968850ffaa7d1
Mathieu Leplatre <mathieu@mozilla.com>
2024-02-02 17:54:48 +0100

    Check pandoc install in healthcheck

--- bin/healthcheck.py
+++ bin/healthcheck.py
@@ -12,5 +12,9 @@ session = requests.Session()
 retries = Retry(total=5, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
 session.mount("http://", HTTPAdapter(max_retries=retries))
 if __name__ == "__main__":
-    response = session.get(f"http://0.0.0.0:{PORT}/")
+    url = f"http://0.0.0.0:{PORT}"
+    response = session.get(f"{url}/")
     response.raise_for_status()
+
+    hb_response = session.get(f"{url}/__heartbeat__")
+    assert hb_response.json["pandoc_install"]
