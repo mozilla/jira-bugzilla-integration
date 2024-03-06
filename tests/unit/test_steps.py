0ecdf4ddf1a8a0e7f1fac84fe994f3bd8062d783
Mathieu Leplatre <mathieu@mozilla.com>
2024-03-06 10:47:49 +0100

    Add missing test

--- tests/unit/test_steps.py
+++ tests/unit/test_steps.py
@@ -897,6 +897,42 @@ def test_update_issue_points(
     )
 
 
+def test_update_issue_points_missing_in_map(
+    action_context_factory,
+    mocked_jira,
+    action_params_factory,
+    webhook_event_change_factory,
+    capturelogs,
+):
+    action_context = action_context_factory(
+        operation=Operation.UPDATE,
+        current_step="maybe_update_issue_points",
+        bug__see_also=["https://mozilla.atlassian.net/browse/JBI-234"],
+        jira__issue="JBI-234",
+        bug__cf_fx_points="42",
+        event__action="modify",
+        event__changes=[
+            webhook_event_change_factory(field="cf_fx_points", removed="?", added="42")
+        ],
+    )
+
+    params = action_params_factory(
+        jira_project_key=action_context.jira.project,
+    )
+
+    with capturelogs.for_logger("jbi.steps").at_level(logging.DEBUG):
+        result, _ = steps.maybe_update_issue_points(
+            action_context, parameters=params, jira_service=JiraService(mocked_jira)
+        )
+
+    assert result == steps.StepStatus.INCOMPLETE
+    mocked_jira.create_issue.assert_not_called()
+    mocked_jira.update_issue_field.assert_not_called()
+    assert capturelogs.messages == [
+        "Bug cf_fx_points '42' was not in the cf_fx_points map."
+    ]
+
+
 @pytest.mark.parametrize(
     "project_components,bug_component,config_components,expected_jira_components,expected_logs",
     [
