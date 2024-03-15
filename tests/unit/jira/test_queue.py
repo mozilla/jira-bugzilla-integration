9e77e708a6451143dfe87c48b4edec609c045034
Graham Beckley <gbeckley@mozilla.com>
2024-03-15 12:00:12 -0400

    Add some basic, happy path tests for the DeadLetterQueue

--- tests/unit/jira/test_queue.py
+++ tests/unit/jira/test_queue.py
@@ -1,17 +1,90 @@
-import tempfile
+from datetime import datetime, timedelta
 
 import pytest
 
 from jbi.queue import DeadLetterQueue
 
+pytestmark = [pytest.mark.asyncio]
 
-@pytest.mark.parametrize(
-    "dsn",
-    [("file://" + tempfile.mkdtemp()), ("memory://")],
-)
-@pytest.mark.asyncio
-async def test_basic_queue_features(dsn, webhook_request_factory):
-    queue = DeadLetterQueue(dsn)
+
+@pytest.fixture
+def memory_dlq():
+    return DeadLetterQueue("memory://")
+
+
+@pytest.fixture
+def filesystem_dlq(tmp_path):
+    return DeadLetterQueue("file://" + str(tmp_path))
+
+
+@pytest.fixture(params=["memory_dlq", "filesystem_dlq"])
+def queue(request):
+    queue = request.getfixturevalue(request.param)
+    return queue
+
+
+async def test_postpone(queue: DeadLetterQueue, webhook_request_factory):
+    webhook_payload = webhook_request_factory()
+
+    await queue.postpone(webhook_payload)
+
+    [event] = await queue.backend.get(webhook_payload.bug.id)
+    assert event.payload == webhook_payload
+
+
+async def test_track_failed(queue: DeadLetterQueue, webhook_request_factory):
+    webhook_payload = webhook_request_factory()
+    exc = Exception("boom")
+
+    await queue.track_failed(webhook_payload, exc)
+    [event] = await queue.backend.get(webhook_payload.bug.id)
+
+    assert event.payload == webhook_payload
+    assert event.error.description == str(exc)
+
+
+async def test_is_blocked(
+    queue: DeadLetterQueue, queue_item_factory, webhook_request_factory
+):
+    blocked_payload = webhook_request_factory(bug__id=123)
+    item = queue_item_factory(payload=blocked_payload)
+    await queue.backend.put(item)
+
+    assert await queue.is_blocked(blocked_payload) is True
+
+    another_payload = webhook_request_factory(bug__id=456)
+    assert await queue.is_blocked(another_payload) is False
+
+
+async def test_retrieve(queue: DeadLetterQueue, queue_item_factory):
+    bug_ids = (1, 2, 1, 3)
+    now = datetime.now()
+
+    for idx, bug_id in enumerate(bug_ids):
+        timestamp = now + timedelta(minutes=idx)
+        await queue.backend.put(
+            queue_item_factory(
+                timestamp=timestamp,
+                payload__event__time=timestamp,
+                payload__bug__id=bug_id,
+            )
+        )
+
+    items = await queue.retrieve()
+    assert [item.payload.bug.id for item in items] == [1, 1, 2, 3]
+
+
+async def test_done(queue: DeadLetterQueue, queue_item_factory):
+    item = queue_item_factory()
+
+    await queue.backend.put(item)
+    assert await queue.backend.size() == 1
+
+    await queue.done(item)
+    assert await queue.backend.size() == 0
+
+
+async def test_basic_queue_features(queue, webhook_request_factory):
     assert await queue.backend.size() == 0
 
     # Track a failure.
