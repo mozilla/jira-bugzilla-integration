a75b9b0fb8478c0036e55a8120547f9f910216a9
Mathieu Leplatre <mathieu@mozilla.com>
2024-04-24 13:53:59 +0200

    Fix #969: force message on HTTPError when empty

--- jbi/jira/client.py
+++ jbi/jira/client.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from typing import Collection, Iterable, Optional
 
@@ -62,6 +63,21 @@ class JiraClient(Jira):
                 response.reason,
                 extra={"body": response.text},
             )
+            if str(exc) == "":
+                # Some Jira errors are raised as `HTTPError('')`.
+                # We are trying to turn them into insightful errors here.
+                if response is not None:
+                    try:
+                        content = exc.response.json()
+                        errors = content.get("errors", {})
+                        response_details = ",".join(
+                            f"{k}: {v}" for k, v in errors.items()
+                        )
+                    except json.JSONDecodeError:
+                        response_details = exc.response.text
+                    # Set the exception message so that its str version contains details.
+                    msg = f"HTTP {exc.response.status_code}: {response_details}"
+                    exc.args = (msg,) + exc.args[1:]
             raise
 
     get_server_info = instrumented_method(Jira.get_server_info)
