34b87b9888653c40265787b09be6d89e5dc67cfe
Alex Cottner <acottner@mozilla.com>
2024-04-25 10:08:46 -0600

    Adding comment to explain why we do the regex match

--- jbi/queue.py
+++ jbi/queue.py
@@ -205,7 +205,7 @@ class FileBackend(QueueBackend):
     async def get_all(self) -> dict[int, AsyncIterator[QueueItem]]:
         all_items: dict[int, AsyncIterator[QueueItem]] = {}
         for filesystem_object in self.location.iterdir():
-            if filesystem_object.is_dir() and re.match("\d", filesystem_object.name):
+            if filesystem_object.is_dir() and re.match("\d", filesystem_object.name): # filtering out temp files from checks
                 all_items[int(filesystem_object.name)] = self.get(filesystem_object)
         return all_items
 
