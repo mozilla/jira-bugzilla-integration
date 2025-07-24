6be6859fff0c1e8183ba25ba1bf4ba7438c894ca
Mathieu Leplatre <mathieu@mozilla.com>
2025-07-24 11:30:58 +0200

    Ref #984: Show more details about attachment when created  (#1167)
    Parse attachment info when context.event.target == "attachment"

--- tests/conftest.py
+++ tests/conftest.py
@@ -171,6 +171,7 @@ def context_attachment_example(action_context_factory) -> ActionContext:
     return action_context_factory(
         operation=Operation.ATTACHMENT,
         bug__see_also=["https://mozilla.atlassian.net/browse/JBI-234"],
+        bug__with_attachment=True,
         event__target="attachment",
         event__routed_key="attachment.create",
         event__user__login="phab-bot@bmo.tld",
