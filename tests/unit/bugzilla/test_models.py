c98dc59b7fe7804f2870664a1c93e47ed8c32303
Segun Famisa <sfamisa@mozilla.com>
2025-10-17 12:57:59 +0200

    Add test case for patch attachment with unexpected file name

--- tests/unit/bugzilla/test_models.py
+++ tests/unit/bugzilla/test_models.py
@@ -20,9 +20,17 @@ def test_patch_attachment_returns_valid_phabricator_url(bug_factory):
 def test_non_patch_attachment_returns_no_phabricator_url(bug_factory):
     bug = bug_factory.build(
         with_attachment=True,
-        attachment__is_patch=False,
         attachment__file_name="screenshot.png",
         attachment__content_type="image/png",
+    )
+    base_url = "https://phabricator.services.mozilla.com"
+    assert bug.attachment.phabricator_url(base_url=base_url) is None
+
+def test_patch_attachment_with_unexpected_file_name_returns_no_phabricator_url(bug_factory):
+    bug = bug_factory.build(
+        with_attachment=True,
+        attachment__file_name="phabricator-F262912-url.txt", # normally should be "phabricator-D<>-url.txt"
+        attachment__content_type="text/x-phabricator-request",
     )
     base_url = "https://phabricator.services.mozilla.com"
     assert bug.attachment.phabricator_url(base_url=base_url) is None
